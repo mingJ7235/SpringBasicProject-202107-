58a089a6be0ea71fbcf5ea51ecb0f56f0a4a50bc
mingJ7235 <joshuara7235@gmail.com>
2021-07-17 19:40:50 +0900

    Refactor add code

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,12 +9,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
     //DI 해주는 것이다.
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
